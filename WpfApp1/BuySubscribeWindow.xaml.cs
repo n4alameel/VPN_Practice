b7c07a2683996f390069fc072da321885b5b6d7a
Марія Петрова <Mariia.Petrova@o365.opu.ua>
2022-02-14 15:30:52 +0200

    Сделано окно и кнопки для покупки премиума

---
+++ WpfApp1/BuySubscribeWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для BuySubscribeWindow.xaml
+    /// </summary>
+    public partial class BuySubscribeWindow : Window
+    {
+        public BuySubscribeWindow()
+        {
+            InitializeComponent();
+            Choice.IsChecked = true;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Close();
+            this.Close();
+        }
+
+        private void Hide_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+
+        private void Choice_Click(object sender, RoutedEventArgs e)
+        {
+            if (Upgrade.IsChecked == true) Upgrade.IsChecked = false;
+            const string url = "https://my.keepsolid.com/pricing/buy/35/?platform=Win&purchase_id=35#prch35";
+            Process.Start(url);
+        
+        }
+
+        private void Upgrade_Click(object sender, RoutedEventArgs e)
+        {
+            if (Choice.IsChecked == true) Choice.IsChecked = false;
+            const string url = "https://www.keepsolid.com/monodefense/pricing?price=26440";
+            Process.Start(url);
+        
+        }
+
+        private void nonClientArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void WhatIsMD_Click(object sender, RoutedEventArgs e)
+        {
+            const string url = "https://www.keepsolid.com/monodefense/ru/what-is-md";
+            Process.Start(url);
+        }
+
+        private void Continue_Click(object sender, RoutedEventArgs e)
+        {
+            if(Upgrade.IsChecked == true)
+            {
+                const string url = "https://www.keepsolid.com/monodefense/pricing?price=26440";
+                Process.Start(url);
+            }
+            else if (Choice.IsChecked == true)
+            {
+                const string url = "https://my.keepsolid.com/pricing/buy/35/?platform=Win&purchase_id=35#prch35";
+                Process.Start(url);
+            }
+        }
+    }
+}
--- WpfApp1/PremiumWindow.xaml.cs
+++ WpfApp1/PremiumWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,6 +23,76 @@ namespace WpfApp1
         public PremiumWindow()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Hide_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+        private void nonClientArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+
+        private void _1year_Click(object sender, RoutedEventArgs e)
+        {
+            BuySubscribeWindow buySubscribeWindow = new BuySubscribeWindow();
+            buySubscribeWindow.Owner = this;
+            this.Hide();
+            buySubscribeWindow.Show();
+            buySubscribeWindow.Choice.Content = " 1 год \n $69.99 \n Оплачивается ежегодно";
+            buySubscribeWindow.Upgrade.Content = "MonoDefense 1 год \n $99.99 \n Оплачивается ежегодно";
+        }
+
+        private void Infinity_Click(object sender, RoutedEventArgs e)
+        {
+            BuySubscribeWindow buySubscribeWindow = new BuySubscribeWindow();
+            buySubscribeWindow.Owner = this;
+            this.Hide();
+            buySubscribeWindow.Show();
+            buySubscribeWindow.Choice.Content = " Вечность \n $99.99 \n Оплачивается единоразово";
+            buySubscribeWindow.Upgrade.Content = "MonoDefense вечность \n $199.99 \n Оплачивается единоразово";
+        }
+
+        private void _7days_Click(object sender, RoutedEventArgs e)
+        {
+            BuySubscribeWindow buySubscribeWindow = new BuySubscribeWindow();
+            buySubscribeWindow.Owner = this;
+            this.Hide();
+            buySubscribeWindow.Show();
+            const string url = "https://my.keepsolid.com/pricing/buy/130/?platform=Win&purchase_id=130#prch130";
+            Process.Start(url);
+        }
+
+        private void _1month_Click(object sender, RoutedEventArgs e)
+        {
+            BuySubscribeWindow buySubscribeWindow = new BuySubscribeWindow();
+            buySubscribeWindow.Owner = this;
+            this.Hide();
+            buySubscribeWindow.Show();
+            buySubscribeWindow.Choice.Content = "1 месяц \n $11.99 \n Оплачивается ежемесячно";
+            buySubscribeWindow.Upgrade.Content = "MonoDefense 1 месяц \n $14.99 \n Оплачивается ежемесячно";
         }
     }
 }
